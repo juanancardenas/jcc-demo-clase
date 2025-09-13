755e3cfe9a358e29375c150ae93c901c019a0470
jacardenas <juanancardenas@gmail.com>
2025-09-13 09:36:22 +0200

    add C3 #1

--- src/main/java/es/upm/miw/devops/C2.java
+++ src/main/java/es/upm/miw/devops/C2.java
@@ -1,4 +1,14 @@
 package es.upm.miw.devops;
 
 public class C2 {
+
+    private double atrr1;
+
+    public double getAtrr1() {
+        return atrr1;
+    }
+
+    public void setAtrr1(double atrr1) {
+        this.atrr1 = atrr1;
+    }
 }
